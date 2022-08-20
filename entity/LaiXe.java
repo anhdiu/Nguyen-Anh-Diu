b134ace81778efca2ee4951dac0c4c2239dd77ca
anhdiu <anhdiu153@gmail.com>
2022-08-20 22:10:25 +0900

    diucommit

--- entity/LaiXe.java
+++ entity/LaiXe.java
@@ -1,6 +1,6 @@
 package entity;
 
-public class LaiXe {
+public abstract class LaiXe {
     private static int maLaiXe_Id = 10000;
     private String hoTen;
     private String DiaChi;
@@ -14,12 +14,19 @@ public class LaiXe {
         this.trinhDo = trinhDo;
     }
 
+    public LaiXe() {
+
+    }
+
     public static int maLaiXe_Id() {
         return maLaiXe_Id;
     }
 
     public static void setMaLaiXe_Id(int maLaiXe_Id) {
         LaiXe.maLaiXe_Id = maLaiXe_Id;
+    }
+
+    protected static void inputInfo() {
     }
 
     public String hoTen() {
@@ -63,5 +70,14 @@ public class LaiXe {
                 ", trinhDo='" + trinhDo + '\'' +
                 '}';
     }
+    public int CompareTo(LaiXe laiXe) {
+        if(this.maLaiXe_Id>laiXe.maLaiXe_Id()) {
+            return 1;
+        }else if (this.maLaiXe_Id<laiXe.maLaiXe_Id()) {
+            return -1;
+        }
+        return 0;
+    }
+
 
 }
